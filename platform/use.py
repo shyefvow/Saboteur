d96fb98298fa33b12123ab3dc8a6b6d7c708c26b
shyefvow <66862572+shyefvow@users.noreply.github.com>
2023-04-06 15:49:36 +0800

    Update use.py

--- platform/use.py
+++ platform/use.py
@@ -65,14 +65,22 @@ model.eval()
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=False, download=True,
         transform=transforms.Compose([transforms.ToTensor(), ])),
-    batch_size=batch_size, shuffle=False )
+    batch_size=batch_size, shuffle=False)
 
 iter_times = 3
 plt.figure(figsize=(24, 2.5*iter_times))
 cnt = 0
+correct = 0
+total = 0
 for i, (data, target) in enumerate(test_loader):
     attack = PGDAttack(model)
     data = attack(data, target)
+    # 计算准确率
+    predict = model(data.to(device))
+    _, predicted = torch.max(predict.data, 1)
+    total += target.size(0)
+    correct += (predicted == target.to(device)).sum().item()
+
     for j in range(batch_size):
         cnt += 1
         plt.subplot(iter_times, batch_size,cnt)
@@ -81,10 +89,11 @@ for i, (data, target) in enumerate(test_loader):
 
         plt.imshow(data.detach().cpu().numpy()[j, :, :, :].reshape(28, 28), cmap='gray')
 
-        predict = model(data.to(device))
         m = nn.Softmax()
         soft_output = m(predict)
         plt.title('{} -> {}'.format(target[j], soft_output.argmax(axis=1)[j]))
     if (i >= iter_times-1):
         break
 plt.show()
+acc = correct/total
+print(acc)
